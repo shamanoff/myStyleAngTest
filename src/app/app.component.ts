3944b9b1ea121a4923256c1f3ec1195f4016c293
shamanoff <shamanoff@gmail.com>
2017-04-24 17:00:19 +0300

    work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
+  divClicked = false;
+  doAnimate = false;
+  ifover = false;
+
+
+  title:string= 'hay';
+  body: string='world';
+
+
+
 }
